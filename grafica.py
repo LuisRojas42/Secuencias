278cb6229cc0d1f73a082ec0703552c0f90b13ec
LuisRojas42 <60357728+LuisRojas42@users.noreply.github.com>
2020-05-22 21:37:35 -0500

    Add files via upload

---
+++ grafica.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+
+def grafica(x, xn0, h, hn0, y, yn0):
+    gs = gridspec.GridSpec(2,2)
+    
+
+    aux = 0
+    xy = []
+    for i in x:
+        xy.append(aux - xn0)
+        aux += 1
+    plt.subplot(gs[0,0])
+    plt.bar(xy, x, width=0.05)
+    plt.xlabel('n')
+    plt.ylabel('x(n)')
+    plt.grid()
+    plt.xticks(np.arange(min(xy), max(xy)+1, step=1))
+    plt.yticks(np.arange(min(x), max(x)+1, step=1))
+
+    aux = 0
+    hy = []
+    for i in h:
+        hy.append(aux - hn0)
+        aux += 1
+    plt.subplot(gs[0,1])
+    plt.bar(hy, h, width=0.05)
+    plt.xlabel('n')
+    plt.ylabel('h(n)')
+    plt.grid()
+    plt.xticks(np.arange(min(hy), max(hy)+1, step=1))
+    plt.yticks(np.arange(min(h), max(h)+1, step=1))
+
+    aux = 0
+    yy = []
+    for i in y:
+        yy.append(aux - yn0)
+        aux += 1
+    plt.subplot(gs[1, :])
+    plt.bar(yy, y, width=0.05)
+    plt.xlabel('n')
+    plt.ylabel('y(n)')
+    plt.grid()
+    plt.xticks(np.arange(min(yy), max(yy)+1, step=1))
+    if max(y) < 20: 
+        plt.yticks(np.arange(min(y), max(y)+1, step=1))
+    elif max(y) < 40:
+        plt.yticks(np.arange(min(y), max(y)+1, step=5))
+    elif max(y) < 60:
+        plt.yticks(np.arange(min(y), max(y)+1, step=10))
+    elif max(y) < 80:
+        plt.yticks(np.arange(min(y), max(y)+1, step=15))
+    elif max(y) < 100:
+        plt.yticks(np.arange(min(y), max(y)+1, step=20))
+
+    # MOSTRAR LEYENDA, CUADRICULA Y FIGURA
+    plt.legend()
+    plt.show()
+
